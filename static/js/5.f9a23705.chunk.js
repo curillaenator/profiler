52bcbd929c466971cde928fe54f7d66c90155f25
curillaenator <curillaenator@gmail.com>
2021-02-06 22:20:51 +0300

    Updates

---
+++ static/js/5.f9a23705.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpprofiler=this.webpackJsonpprofiler||[]).push([[5],{103:function(i,r,e){"use strict";e.r(r);var s=e(0);r.default=function(){return Object(s.jsx)("div",{children:"Skills"})}}}]);
+//# sourceMappingURL=5.f9a23705.chunk.js.map
---
+++ static/js/6.b6a59004.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpprofiler=this.webpackJsonpprofiler||[]).push([[6],{104:function(r,e,i){"use strict";i.r(e);var s=i(0);e.default=function(){return Object(s.jsx)("div",{children:"Works"})}}}]);
+//# sourceMappingURL=6.b6a59004.chunk.js.map
